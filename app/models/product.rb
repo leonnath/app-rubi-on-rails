fc2c62cd5dbfc55741fb06eaaebce2923067dec4
Natalia Garcia <nagarcia.docente@gmail.com>
2022-08-26 18:14:27 +0200

    add the function add and subtract total of the movements

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,17 @@
 class Product < ApplicationRecord
-    has_many :movements
     validates :name, presence: true
+    validates :reference, presence: true
+    has_many :movements
 
     def quantity 
-        movement.sum(:quantity)
+        total = 0
+        self.movements.each do |movement|
+            if movement.movement_type == Movement::MovementTypes[:add]
+                total += movement.quantity
+            else 
+                total -= movement.quantity
+            end
+        end
+        return total
     end
 end
